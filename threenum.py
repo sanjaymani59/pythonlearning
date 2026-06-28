d3cf3e01c937f208bc911b4bdfb5bef7b3d77a99
sanjay mani <sanjaymani9443@gmail.com>
2026-06-28 23:28:45 +0530

    DAY 1 py

---
+++ threenum.py
@@ -0,0 +1,10 @@
+a=int(input("enter number a:"))
+b=int(input("enter number b:"))
+c=int(input("enter number c:"))
+
+if a<=b and a<=c:
+    print("smallest a:");
+elif b<=a and b<=c:
+    print("smallest b:");
+else:
+    print("smallest c:") 
