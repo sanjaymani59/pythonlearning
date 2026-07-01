5d40518ebf3518a087258665f3d00109a01287b6
sanjay mani <sanjaymani9443@gmail.com>
2026-07-01 15:56:02 +0530

    randomguessgame

--- gamenestedif.py
+++ gamenestedif.py
@@ -1,7 +1,10 @@
+import random
+
 print("welcome to number guess game ")
 print("enter the number 1-100")
 
-secter_number=59
+secter_number = random.randint(1,100)
+
 
 for i in range(1,11):
     user_input=int(input("enter a number:"))
@@ -17,5 +20,10 @@ for i in range(1,11):
     else:
         print("you guess is correct")
         break
+
+if secter_number==user_input:
+     print(f"congrulation you guess the numbe  in {secter_number} in {i} attempts")
+else:
+    print("your guessing chance is over better luck next time")
     
     
