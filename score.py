f2c608863b1c92a35bed9356601ded5ac8143fc0
sanjay mani <sanjaymani9443@gmail.com>
2026-07-05 09:26:05 +0530

    score code

---
+++ score.py
@@ -0,0 +1,12 @@
+score=int(input("enter th score"));
+
+if(score<35):   
+    print("fail")
+elif(score>=35 and score<=70):
+     
+     print ("goood score")
+elif(score>70 and score<=100):
+    print("very gooooog")
+
+else:
+    print("invalide score")
