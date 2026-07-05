c4ca0c3f4adcbeb7ed29e3f8301c3c5cd16736c4
sanjay mani <sanjaymani9443@gmail.com>
2026-07-05 22:10:42 +0530

    class_02

---
+++ class_02.py
@@ -0,0 +1,10 @@
+class dog:
+    def __init__(self,name,age,brak):
+        self.name=name
+        self.age=age
+        self.brak=brak
+
+d1=dog("buddy","mee","woofe")
+print(d1.name)
+print(d1.age)
+print(d1.brak)
