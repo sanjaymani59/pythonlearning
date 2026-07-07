25c6071e6188b6cda36dffeb359fc5e0b576cd2c
sanjay mani <sanjaymani9443@gmail.com>
2026-07-07 23:02:24 +0530

    listcreate_01

--- listcreate.py
+++ listcreate.py
@@ -1,6 +1,11 @@
 a=[]
 for i in range(10):
-    num=int(input("enter a number:"))
-
+    num=int(input("enter a number:"+str(i+1)))
     a.append(num)
 print(a)
+
+sum =0
+
+for i in a:
+    sum=sum+i
+print(sum)
