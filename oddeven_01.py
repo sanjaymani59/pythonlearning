32c69ab23f5a1901b87204126bd5c79ebd107aa5
sanjay mani <sanjaymani9443@gmail.com>
2026-07-20 20:54:37 +0530

    odd even

---
+++ oddeven_01.py
@@ -0,0 +1,16 @@
+ 
+# count=0
+# for i in range(5,16):
+#     if(i%3==0):
+#         count=count+1;
+    
+       
+# print(count)  
+
+
+i =10
+while(i>0):
+    print(i, end=",")
+    i=i-1
+    
+  
