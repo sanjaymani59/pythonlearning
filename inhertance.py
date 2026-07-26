dfafc35dc677745720c6ae51a7af81ca965e188a
sanjay mani <sanjaymani9443@gmail.com>
2026-07-26 19:20:31 +0530

    inhertance

---
+++ inhertance.py
@@ -0,0 +1,13 @@
+class dog:
+
+    class eat():
+        # def __init__(self):
+            print("dog eating the food ")
+
+    class brak():
+        print("after eating dog started barking")
+
+dog1=dog()
+
+dog1.eat()
+dog1.brak()
