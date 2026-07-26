cd36613cd5e815df35afc54c5f63701b6aac4027
sanjay mani <sanjaymani9443@gmail.com>
2026-07-26 18:24:01 +0530

    instance

---
+++ instance.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+
+student1=student("sam",50)
+student2=student("jayam",56)
+
+print(student1.name)
+print(student1.age)
+print(student2.name)
+print(student2.age)
