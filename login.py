953a820a6755db82afba1eaac4e74c15c462eb98
sanjay mani <sanjaymani9443@gmail.com>
2026-08-04 23:48:40 +0530

    login code

---
+++ login.py
@@ -0,0 +1,10 @@
+username=input("Enter Your Username:")
+password=input("Enter YOur password:")
+
+if (username=="sam"):
+    if(password=="1234"):
+        print("you successfully login")
+    else:
+        print("password is wrong")
+else:
+    print("username is wrong")
