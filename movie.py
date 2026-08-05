f3b89c6f583493a561b99c2092039f1a258c7448
sanjay mani <sanjaymani9443@gmail.com>
2026-08-06 01:07:30 +0530

    movie ticket

---
+++ movie.py
@@ -0,0 +1,11 @@
+age=int(input("Enter the age :"))
+avaliable=True
+
+if(avaliable):
+    if(age>18):
+        print("ticket is booked")
+    else:
+        print("your not a adult")
+
+else:
+    print("ticket is fulled")
