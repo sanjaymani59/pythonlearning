dbebb47be5e7b36e0475833deb4bd703b578dec2
sanjay mani <sanjaymani9443@gmail.com>
2026-08-06 23:27:30 +0530

    match and case

---
+++ match.py
@@ -0,0 +1,24 @@
+name=int(input("Enter The Number Between(1-7):"))
+
+match name:
+    case 1:
+        print ("monday")
+
+    case 2:
+        print("thusday")
+
+    case 3:
+        print("wedneaday")
+
+    case 4:
+        print("thursday" )
+    case 5:
+        print("friday")
+    case 6:
+        print("saturdaay")
+
+    case 7:
+        print("sunday")
+
+    case _:
+        print("invalide")
