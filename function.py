a0deb6b4d1ca4990ca929feea22e9b7260570b6c
sanjay mani <sanjaymani9443@gmail.com>
2026-08-08 03:18:34 +0530

    function all basics

---
+++ function.py
@@ -0,0 +1,20 @@
+""" def greet():
+    print("Hello Function")
+greet() 
+
+def welcome():
+    print("Hello,multiple welcomes")
+welcome()
+welcome()
+welcome() """
+
+def name(name):
+    print("Name",name)
+
+name("sam")
+name("sandy")
+
+def add(a,b):
+    print("add a+b:",a+b)
+add(59,59)
+add(89,48)
