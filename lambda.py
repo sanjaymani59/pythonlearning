7b1d85a8fc86a1e5e8b6d7344e6434be1ab85a7e
sanjay mani <sanjaymani9443@gmail.com>
2026-08-08 23:22:10 +0530

    parameters

---
+++ lambda.py
@@ -0,0 +1,11 @@
+""" square=lambda X: X*X
+
+print(square(6))
+ """
+
+""" def total(*number):
+    print(sum(number))
+
+total(10,20)
+total(10,20,30,40) """
+
---
+++ parameters.py
@@ -0,0 +1,34 @@
+def greet(name):
+    print ("hello",name)
+
+greet("sanjay")
+greet("me it self")
+
+def add(a,b):
+    print(a+b)
+
+add(10,45)
+
+
+def student(name,age,dept):
+    print("name",name)
+    print("age",age)
+    print("dept",dept)
+
+student("sam",43,"cse")
+
+def multeply(a,b):
+    return a*b
+
+result =multeply(4,65)
+print(result)
+
+
+def greet(name="student"):
+    print(name)
+
+greet()
+greet("sanjay")
+
+
+
