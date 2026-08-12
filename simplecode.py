f6186eecc2e5e38b7cb9c39f048278403b162247
sanjay mani <sanjaymani9443@gmail.com>
2026-08-13 00:34:26 +0530

    star into num

--- simplecode.py
+++ simplecode.py
@@ -10,7 +10,12 @@ print("good buy")
  """
 
 
-for i in range(1,6):
+"""for i in range(1,6):
     for j in range(i):
         print("*",end="")
+    print() """
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
     print()
