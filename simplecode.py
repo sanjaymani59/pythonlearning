b43f33057a12f376e58009515718d23b2bc730d3
sanjay mani <sanjaymani9443@gmail.com>
2026-08-14 00:54:24 +0530

    add

--- simplecode.py
+++ simplecode.py
@@ -15,7 +15,18 @@ print("good buy")
         print("*",end="")
     print() """
 
-for i in range(1,6):
+""" for i in range(1,6):
     for j in range(1,i+1):
         print(j,end="")
-    print()
+    print() """
+
+
+for i in range (1,6):
+    if i==5:
+        break
+
+    for j in range(1,6):
+        if i==3:
+            continue
+
+        print(i,j)
