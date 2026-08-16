70fe75ce9b8a730c85c8a7e7a3c5ee40da3c4e31
sanjay mani <sanjaymani9443@gmail.com>
2026-08-17 00:39:12 +0530

    simple

--- newtax.py
+++ newtax.py
@@ -15,5 +15,20 @@ print (f"your tax is: ${final_tax}")
 """ check = lambda x:"pos" if x>0 else "neg"
 print (check(10)) """
 
-check=lambda x:"even " if x%2==0 else "odd"
-print(check(5))
+""" check=lambda x:"even " if x%2==0 else "odd"
+print(check(5)) """
+
+
+larger=lambda a,b: a if a>b else b
+print (larger(9,4 ))
+
+
+multiplay=lambda a,b: a*b
+print(multiplay(5,5))
+
+
+sub=lambda a,b:a-b
+print (sub(5,5))
+
+def muktiplay(a,b):
+    return a*b
