8a0b8deab3d441f3eb353c7bbd13360a7a539319
sanjay mani <sanjaymani9443@gmail.com>
2026-08-17 08:56:12 +0530

    gate code

--- newtax.py
+++ newtax.py
@@ -19,7 +19,7 @@ print (check(10)) """
 print(check(5)) """
 
 
-larger=lambda a,b: a if a>b else b
+""" larger=lambda a,b: a if a>b else b
 print (larger(9,4 ))
 
 
@@ -31,4 +31,26 @@ sub=lambda a,b:a-b
 print (sub(5,5))
 
 def muktiplay(a,b):
-    return a*b
+    return a*b """
+
+def total(old_avg,new_avg,incorr,corr):
+
+    total_cha=corr-incorr
+
+    total_avg=new_avg-old_avg
+
+
+    if avg==0:
+        raise ValueError ("Average is not found")
+
+    avge=total_cha/total_cha
+
+    return int (avg)
+o_a=59
+n_a=56
+in_cor=4
+cor=6
+
+total_std=total(o_a,n_a,in_cor,cor)
+
+print(f"total student:{total_std}")
