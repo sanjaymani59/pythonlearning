79c75f934282a75e9754bd6920855fb5eb7945be
sanjay mani <sanjaymani9443@gmail.com>
2026-08-19 00:34:35 +0530

    simple loop

--- random.py
+++ random.py
@@ -1,6 +1,6 @@
 """ import random
 num=random.randint(0,9)
-print("Random:",num)"""  
+print("Random:",num)
 
 def add(a,b):
     return a+b
@@ -21,4 +21,28 @@ def check(age):
     return "not Eligible"
 
 print(check(20))
-print(check(15))
+print(check(15)) """
+
+
+""" def list_nnum():
+    return[10,43]
+num =list_nnum
+
+print(num) """
+
+
+def number(num,target):
+    for nums in num:
+
+        if num==target:
+            return "found"
+
+        return "not Found"
+
+num=[45,4,43,6,2,]
+
+
+print(number(num,4))
+
+ 
+
