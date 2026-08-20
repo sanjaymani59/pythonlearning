1af0f2ba839b52dea3ed6dcdad71dea8dc65f766
sanjay mani <sanjaymani9443@gmail.com>
2026-08-21 01:20:25 +0530

    golbal scop

--- local&goble.py
+++ local&goble.py
@@ -1,24 +1,77 @@
 
 """ //local variable or scope """
-def test():
+""" def test():
     x=20
     print(x)
 
-test()
+test() """
 
 """ global scope """
 
-x=59
+""" x=59
 def test():
     print(x)
-test()
+test() """
 
 
 """ both local and global  """
-x=59
+""" x=59
 def test():
     x=30
     print("inside:",x)
 test()
 
-print("outside:",x)
+print("outside:",x) """
+
+
+
+
+""" def change():
+    global x
+    x=6
+change()
+print(x)
+
+
+def student(name):
+    print("student:"+name)
+
+student("sam")
+ """
+
+college="sam college"
+
+""" def student():
+    print ("student belongs to "+college)
+
+def stuff():
+    print("stuff belongs to "+college)
+
+student()
+stuff() """
+
+""" for i in range(3):
+    x=i
+print(x) """
+
+""" x=3
+def outer():
+    def inner():
+        x=9
+        print(x)
+    inner()
+print(x)
+outer() """
+
+count=0
+
+def increase():
+    global count
+    count+=1
+    print(count)
+
+increase()
+increase()
+increase()
+increase()
+
