93e8efefaa757f57f979f7da7b29da46ac110901
sanjay mani <sanjaymani9443@gmail.com>
2026-09-23 22:30:59 +0530

    function __02

---
+++ function_in_python.py
@@ -0,0 +1,28 @@
+""" def check (num):
+    if num>0:
+        return "positive "
+    elif num<0:
+        return "negative"
+    else:
+        return "zero"
+
+print(check(4))
+print(check(-3))
+print(check(0))
+ """
+
+# // Level 7 — Function Returning Multiple Values
+
+""" def cal(a,b):
+    add =a+b
+    sub=a-b
+    multip=a*b
+
+    return add,sub,multip
+
+x,y,z=cal(34,3)
+
+print(x)
+print(y)
+print(z) """
+
